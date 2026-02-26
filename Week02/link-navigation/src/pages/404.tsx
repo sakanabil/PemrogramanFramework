8bd9d112f99c6999576765c5ed962b8c729caaaf
saka <sakanabil1221@gmail.com>
2026-02-26 22:07:08 +0700

    add: Jobsheet 6 (done)

---
+++ Week02/link-navigation/src/pages/404.tsx
@@ -0,0 +1,26 @@
+import styles from "@/styles/404.module.scss";
+const Custom404 = () => {
+  return (
+    <div className={styles.error}>
+      <div className={styles.error__container}>
+        <head>
+          <title>404 - Halaman Tidak Ditemukan</title>
+        </head>
+        <img
+          src="/page-not-found.svg"
+          alt="404"
+          className={styles.error__image}
+        />
+        <h1 className={styles.error__title}>404 - Halaman Tidak Ditemukan</h1>
+        <p className={styles.error__description}>
+          Maaf, halaman yang Anda cari tidak ada.
+        </p>
+        <a href="/" className={styles.error__description__link}>
+          Kembali
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Custom404;
