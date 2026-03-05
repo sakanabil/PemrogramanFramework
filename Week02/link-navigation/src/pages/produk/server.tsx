1bcf837cd2c3e9831e19e4f80829e60451baa06c
saka <sakanabil1221@gmail.com>
2026-03-05 21:02:46 +0700

    add: Jobsheet 9 (done)

---
+++ Week02/link-navigation/src/pages/produk/server.tsx
@@ -0,0 +1,24 @@
+import TampilanProduk from "../views/produk";
+import { ProductType } from "../types/Product.type";
+
+const halamanProdukServer = (props: { products: ProductType[] }) => {
+  const { products } = props;
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilanProduk products={products} />
+    </div>
+  );
+};
+
+export default halamanProdukServer;
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/produk");
+  const response = await res.json();
+  return {
+    props: {
+      products: response.data,
+    },
+  };
+}
